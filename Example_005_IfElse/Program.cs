b6f115aeb7fb7fe5f2e4fbcbad958a2f7dfaeeeb
you <you@example.om>
2023-06-07 21:06:09 +0200

    added If Else

---
+++ Example_005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow, that's Masha");
+}
+else{
+    Console.Write("Hello ");
+    Console.WriteLine(username);
+}
