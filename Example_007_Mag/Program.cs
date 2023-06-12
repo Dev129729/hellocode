490ca26d5593d7a08272d0ce4d8b86af4f2982b8
you <you@example.om>
2023-06-12 15:00:04 +0200

    Task5

---
+++ Example_007_Mag/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+
+Console.SetCursorPosition( xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition( xb, yb);
+Console.WriteLine("+");
---
+++ Task5/Program.cs
@@ -0,0 +1,9 @@
+﻿// 5. Напишите программу, кооторая на вход принимает одно число (n), а на выходе показывает все целые числа в промежутке от -N до N. 
+
+Console.WriteLine("Enter N");
+string n = Console.ReadLine();
+int b = Convert.ToInt32(n);
+for (int i = -b; i<=b; i++)
+{
+    Console.WriteLine(i);
+}
