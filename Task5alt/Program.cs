20f5d27b8ae5838e542661f2631c2c1122aa611b
you <you@example.om>
2023-06-13 21:21:29 +0200

    task5alt added

---
+++ Task5alt/Program.cs
@@ -0,0 +1,8 @@
+﻿// 5. Напишите программу, кооторая на вход принимает одно число (n), а на выходе показывает все целые числа в промежутке от -N до N. 
+
+Console.WriteLine("Enter N");
+int b = Convert.ToInt32(Console.ReadLine());
+for (int i = -b; i<=b; i++)
+{
+    Console.WriteLine(i);
+}
