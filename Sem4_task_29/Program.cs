e5683ae7979b6f7072536468f43e4a5327d47f3f
you <you@example.om>
2023-06-21 20:18:19 +0200

    sem4 task 29 added

---
+++ Sem4_task_29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] array = new int[] {1,2,3,4,5,6,7,8};
+Console.WriteLine(array[0]);
+Console.WriteLine(array[1]);
+Console.WriteLine(array[2]);
+Console.WriteLine(array[3]);
+Console.WriteLine(array[4]);
+Console.WriteLine(array[5]);
+Console.WriteLine(array[6]);
+Console.WriteLine(array[7]);
+ 
