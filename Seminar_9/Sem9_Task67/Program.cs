b7c3265ed2fe817a4d1728d99afceaf33e6d2424
you <you@example.om>
2023-07-08 14:48:49 +0200

    Seminar_9/Sem9_Task67/ added

---
+++ Seminar_9/Sem9_Task67/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу,
+// которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int summ(int n)
+{
+    if (n <= 0 ) return(0);
+    return
+    summ(n/10) + (n%10);
+ 
+}
+
+int result =  summ(number);
+Console.WriteLine(result);
+
