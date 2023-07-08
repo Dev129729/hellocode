b7e8a9a9346dd27ece6f3439437f55fb1bb39fd7
you <you@example.om>
2023-07-08 15:50:48 +0200

    Seminar_9/Sem9_Task64/ added

---
+++ Seminar_9/Sem9_Task64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ShowNumbers(int n, int m)
+{
+    if (n == m) return n;
+    else Console.Write($"{ShowNumbers(n, m + 1)}, ");
+    return m;
+}
+
+if (number > 0)
+{
+    Console.WriteLine(ShowNumbers(number, 1));
+}
+else
+{
+    Console.WriteLine("Inroccent number");
+}
