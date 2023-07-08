8721275f42545b8e4871f5498c3811c7c66b8032
you <you@example.om>
2023-07-08 13:56:34 +0200

    Seminar_9/ added

---
+++ Seminar_9/Se9_Task63/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void ShowNumbers(int n)
+{
+    if (n <=0) return;
+    ShowNumbers(n-1);
+    Console.Write(n);
+}
+
+ShowNumbers(number);
